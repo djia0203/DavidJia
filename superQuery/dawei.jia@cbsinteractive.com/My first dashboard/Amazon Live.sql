73fd349a8c3b1d6fcb1652104147fc1b357e9315
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-15 14:47:12 -0400

    Auto-commit: Wed Jul 15 2020 14:47:07 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Amazon Live.sql
@@ -0,0 +1,4 @@
+select video_series_nm, SUM(duration_min_cnt) as minute, SUM(streams_cnt) as streams
+from `i-dss-ent-data.ent_vw.multi_channels_summary_day`
+where day_dt between '2020-06-27' and '2020-07-14' and site_country_cd = 'US'and source_desc = "Amazon Channels" and content_type_cd = 'LIVE'
+group by 1
