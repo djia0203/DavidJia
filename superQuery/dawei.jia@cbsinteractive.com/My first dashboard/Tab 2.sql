8b9fe905b646e17056be48023cb52c2f2286268a
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-20 10:47:23 -0400

    Auto-commit: Mon Jul 20 2020 10:47:18 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
@@ -0,0 +1,5 @@
+select source_desc, content_type_cd, episode_nm, SUM(duration_min_cnt) as minute, SUM(streams_cnt) as streams
+from `i-dss-ent-data.ent_vw.multi_channels_summary_day`
+where day_dt between '2020-06-27' and '2020-07-14' and (episode_nm like "Match Replay%" or episode_nm like "NWSL%") and site_country_cd = 'US'
+      and video_full_episode_ind = true
+group by 1,2,3
