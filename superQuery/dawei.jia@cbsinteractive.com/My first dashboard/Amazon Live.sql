b4997faa26c8ccd427c62cff7a3f49fbdcc69cf3
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-20 11:46:33 -0400

    Auto-commit: Mon Jul 20 2020 11:46:29 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Amazon Live.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Amazon Live.sql
@@ -1,4 +1,4 @@
 select video_series_nm, episode_nm, video_full_episode_ind, SUM(duration_min_cnt) as minute, SUM(streams_cnt) as streams
 from `i-dss-ent-data.ent_vw.multi_channels_summary_day`
-where day_dt between '2020-06-27' and '2020-07-20' and site_country_cd = 'US'and source_desc = "Amazon Channels" and video_series_nm = 'cbsnwsl'
+where day_dt between '2020-06-27' and '2020-07-20' and site_country_cd = 'US' and source_desc = "Amazon Channels" and (video_series_nm = 'cbsnwsl' or episode_nm like "Match Replay%" or episode_nm like "NWSL%")
 group by 1,2,3
