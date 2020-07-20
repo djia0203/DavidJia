d1de64608edb55f131c1f41b5d8d9c9fbcded383
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-20 14:10:19 -0400

    Auto-commit: Mon Jul 20 2020 14:10:15 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
@@ -1,3 +1,3 @@
 select distinct video_series_nm
 from `i-dss-ent-data.dw_vw.mpx_video_content`
-where day_dt >= '2020-06-27' and video_title like 'Match Replay%'
+where video_title like 'Match Replay%'
