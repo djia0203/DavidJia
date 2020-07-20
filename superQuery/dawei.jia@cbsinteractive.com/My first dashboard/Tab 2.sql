3224596ac9224ec830ad9a524b97ac49eba5a1d1
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-20 14:10:09 -0400

    Auto-commit: Mon Jul 20 2020 14:10:05 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
@@ -1,6 +1,3 @@
-select source_desc, content_type_cd,-- episode_nm, 
-       SUM(duration_min_cnt) as minute, SUM(streams_cnt) as streams
-from `i-dss-ent-data.ent_vw.multi_channels_summary_day`
-where day_dt between '2020-06-27' and '2020-07-20' and (episode_nm like "Match Replay%" or episode_nm like "NWSL%") and site_country_cd = 'US'
-      and video_full_episode_ind = true
-group by 1,2--,3
+select distinct video_series_nm
+from `i-dss-ent-data.dw_vw.mpx_video_content`
+where day_dt >= '2020-06-27' and video_title like 'Match Replay%'
