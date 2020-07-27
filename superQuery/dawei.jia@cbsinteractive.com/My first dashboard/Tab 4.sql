21c6c999411842f2371cf23aea37af8a0561936c
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-27 10:57:52 -0400

    Auto-commit: Mon Jul 27 2020 10:51:48 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 4.sql
@@ -0,0 +1,7 @@
+select count(v69_registration_id_nbr)
+from `i-dss-ent-data.dw_vw.aa_cs_video_detail_day` cs join `i-dss-ent-data.dw_vw.mpx_video_content` mpx on cs.v31_mpx_reference_guid=mpx.mpx_reference_guid
+where video_full_episode_ind = TRUE 
+    and video_show_nm = "National Women's Soccer League"
+    and v69_registration_id_nbr is not null
+    and v9_rsid IN ('cnetcbscomsite','cbsicbsapp', 'cbsicbsott')
+    and day_dt >= "2020-06-27"
