5937b361de36b023e44bd9e6dc72ef2c2cbba7ab
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-27 10:42:27 -0400

    Auto-commit: Mon Jul 27 2020 10:42:17 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Survey.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Survey.sql
@@ -1,4 +1,4 @@
-select v69_registration_id_nbr as regid, video_show_nm, video_title, duration_sec_qty/60 as minutes
+select v69_registration_id_nbr as regid, COUNT(distinct video_title) as FEP
 from `i-dss-ent-data.dw_vw.aa_cs_video_detail_day` cs join `i-dss-ent-data.dw_vw.mpx_video_content` mpx on cs.v31_mpx_reference_guid=mpx.mpx_reference_guid
 where video_full_episode_ind = TRUE 
     and video_show_nm = "National Women's Soccer League"
@@ -6,3 +6,4 @@ where video_full_episode_ind = TRUE
     and v9_rsid IN ('cnetcbscomsite','cbsicbsapp', 'cbsicbsott')
     and day_dt >= "2020-06-27"
     and duration_sec_qty/60 >= 30
+group by 1
