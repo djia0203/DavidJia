b3ecf5f660e71c70036f534490f65574a74dffe2
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-27 10:46:21 -0400

    Auto-commit: Mon Jul 27 2020 10:46:14 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Survey.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Survey.sql
@@ -1,3 +1,4 @@
+--select FEP, COUNT(distinct regid) as count from (
 select v69_registration_id_nbr as regid, COUNT(distinct video_title) as FEP
 from `i-dss-ent-data.dw_vw.aa_cs_video_detail_day` cs join `i-dss-ent-data.dw_vw.mpx_video_content` mpx on cs.v31_mpx_reference_guid=mpx.mpx_reference_guid
 where video_full_episode_ind = TRUE 
@@ -7,3 +8,4 @@ where video_full_episode_ind = TRUE
     and day_dt >= "2020-06-27"
     and duration_sec_qty/60 >= 30
 group by 1
+--group by 1
