66db1ffbb07ad583be623ffa979b2d4a113da4eb
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-28 17:41:47 -0400

    Auto-commit: Tue Jul 28 2020 17:41:36 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/FTAG Perf.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/FTAG Perf.sql
@@ -1,7 +1,7 @@
 SELECT day_dt, replace(ftag,"ftag:","") as ftag, count(distinct visit_session_id) as visits, count(distinct post_visitor_id) as devices,
        COUNT(distinct case when post_evar10_desc = "svod_complete" or post_evar10_desc = "billing|payment complete" then post_visitor_id end) as orders,
-       COUNT(distinct case when event.name = "Instance of eVar52" then event_dt_ut end) as streams
-FROM  `i-dss-ent-data.dw_vw.omniture_event_cdm_cnetcbscomsite`, UNNEST(post_campaign_nm) as ftag, UNNEST(post_event_id_list_desc) as event
+       sum(case when exists (select * from unnest(post_event_id_list_desc) as x where x.name = 'Custom Event 52') then 1 else 0 end) as streams
+FROM  `i-dss-ent-data.dw_vw.omniture_event_cdm_cnetcbscomsite`, UNNEST(post_campaign_nm) as ftag
 WHERE  day_dt = '2020-06-01'
 and ftag like  '%AAM%'
 group by 1,2
