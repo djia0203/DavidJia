0e62d64c9172868260f632091fccac286ed44545
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-28 10:57:37 -0400

    Auto-commit: Tue Jul 28 2020 10:57:30 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/TZ2 IAM Subscribe.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/TZ2 IAM Subscribe.sql
@@ -7,6 +7,7 @@ select "WB_Test", (SELECT count(distinct regid) FROM `i-dss-ent-data.temp_dj.TZS
 from `i-dss-ent-data.ent_vw.subscription_fct`
 where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Test`) and activation_dt >= "2020-06-26" and activation_dt <= "2020-07-01"
 order by 1,2
+union all
 select "Acq_Control", (SELECT count(distinct regid) FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Control`) as count, count(distinct cbs_reg_user_id_cd) as subscribe
 from `i-dss-ent-data.ent_vw.subscription_fct`
 where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Control`) and activation_dt >= "2020-06-26" and activation_dt <= "2020-07-01"
