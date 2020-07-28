ad013c37839a4f570a7018490f7b1cb1ea1b0226
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-28 16:43:17 -0400

    Auto-commit: Tue Jul 28 2020 16:43:02 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/TZ2 IAM Subscribe.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/TZ2 IAM Subscribe.sql
@@ -1,18 +1,18 @@
 select "WB_Control", (SELECT count(distinct regid) FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Control`) as count, count(distinct cbs_reg_user_id_cd) as subscribe
 from `i-dss-ent-data.ent_vw.subscription_fct`
-where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Control`) and activation_dt >= "2020-06-26" and activation_dt <= "2020-07-01"
+where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Control`) and cancel_dt >= "2020-06-26" and cancel_dt <= "2020-07-01"
 
 union all
 select "WB_Test", (SELECT count(distinct regid) FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Test`) as count, count(distinct cbs_reg_user_id_cd) as subscribe
 from `i-dss-ent-data.ent_vw.subscription_fct`
-where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Test`) and activation_dt >= "2020-06-26" and activation_dt <= "2020-07-01"
+where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_WB_Test`) and cancel_dt >= "2020-06-26" and cancel_dt <= "2020-07-01"
 
 union all
 select "Acq_Control", (SELECT count(distinct regid) FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Control`) as count, count(distinct cbs_reg_user_id_cd) as subscribe
 from `i-dss-ent-data.ent_vw.subscription_fct`
-where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Control`) and activation_dt >= "2020-06-26" and activation_dt <= "2020-07-01"
+where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Control`) and cancel_dt >= "2020-06-26" and cancel_dt <= "2020-07-01"
 
 union all
 select "Acq_Test", (SELECT count(distinct regid) FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Test`) as count, count(distinct cbs_reg_user_id_cd) as subscribe
 from `i-dss-ent-data.ent_vw.subscription_fct`
-where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Test`) and activation_dt >= "2020-06-26" and activation_dt <= "2020-07-01"
+where cbs_reg_user_id_cd in (SELECT distinct regid FROM `i-dss-ent-data.temp_dj.TZS2_Sampling_Acq_Test`) and cancel_dt >= "2020-06-26" and cancel_dt <= "2020-07-01"
