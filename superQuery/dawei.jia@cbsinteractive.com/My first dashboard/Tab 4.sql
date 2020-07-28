33ed88b2107ffe930a1e7250920bdf46f4592cd8
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-28 15:08:25 -0400

    Auto-commit: Tue Jul 28 2020 15:08:06 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 4.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 4.sql
@@ -1,4 +1,4 @@
-SELECT day_dt, replace(ftag,"ftag:","") as ftag, count(distinct visit_session_id) as visits
+SELECT day_dt, replace(ftag,"ftag:","") as ftag, count(distinct visit_session_id) as visits, count(distinct post_visitor_id) as devices
 FROM  `i-dss-ent-data.dw_vw.omniture_event_cdm_cnetcbscomsite`, UNNEST(post_campaign_nm) as ftag 
 WHERE  day_dt = '2020-06-01'
 and ftag like  '%AAM%'
