ffc6ca21bba3faf12132f3df6ea064006d590490
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-28 15:21:31 -0400

    Auto-commit: Tue Jul 28 2020 15:21:19 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/FTAG Perf.sql
@@ -0,0 +1,8 @@
+SELECT day_dt, replace(ftag,"ftag:","") as ftag, count(distinct visit_session_id) as visits, count(distinct post_visitor_id) as devices,
+       COUNT(distinct case when post_evar10_desc ="svod_complete" then post_visitor_id end) as orders,
+       COUNT(case when post_evar52_desc is not null then post_visitor_id end) as streams
+FROM  `i-dss-ent-data.dw_vw.omniture_event_cdm_cnetcbscomsite`, UNNEST(post_campaign_nm) as ftag 
+WHERE  day_dt = '2020-06-01'
+and ftag like  '%AAM%'
+group by 1,2
+order by 1,3 desc
