145585802779b6d1902bb279b148cc0aa3b65602
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-29 12:37:40 -0400

    Auto-commit: Wed Jul 29 2020 12:37:33 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 3.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 3.sql
@@ -1,7 +1,3 @@
-select distinct v69_registration_id_nbr
-from `i-dss-ent-data.dw_vw.aa_cs_video_detail_day` cs join `i-dss-ent-data.dw_vw.mpx_video_content` mpx on cs.v31_mpx_reference_guid=mpx.mpx_reference_guid
-where video_full_episode_ind = TRUE 
-    and video_show_nm = "National Women's Soccer League"
-    and v69_registration_id_nbr is not null
-    and v9_rsid IN ('cnetcbscomsite','cbsicbsapp', 'cbsicbsott')
-    and day_dt >= "2020-06-27"
+select reg_cookie, att_level_up1, service
+from `i-dss-ent-data.ent_summary.att_20160201_present`
+where start_dt >= "2020-06-27" and start_dt <= "2020-07-27" and att_level_up1 = "NWSL" and service not in ("Trial","Trial to Paid")
