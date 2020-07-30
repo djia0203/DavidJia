1b9e245abfd4e794eddeaab9b4a462863c8f29a3
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-30 10:43:34 -0400

    Auto-commit: Thu Jul 30 2020 10:43:26 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Attribution.sql
@@ -0,0 +1,6 @@
+select reg_cookie, service, att_level_up1, att_gp
+from `i-dss-ent-data.ent_summary.att_20160201_present`
+where reg_cookie in (
+select distinct reg_cookie
+from `i-dss-ent-data.ent_summary.att_20160201_present`
+where start_dt >= "2020-06-27" and start_dt <= "2020-07-27" and att_level_up1 = "NWSL" and service not in ("Trial","Trial to Paid")) and start_dt < "2020-06-27"
