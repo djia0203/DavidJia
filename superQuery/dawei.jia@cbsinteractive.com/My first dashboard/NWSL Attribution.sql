b1390b69f39b5dfac65b0fe42e0d00bc531b3222
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-07-30 10:44:39 -0400

    Auto-commit: Thu Jul 30 2020 10:44:31 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Attribution.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/NWSL Attribution.sql
@@ -1,6 +1,8 @@
+select att_level_up1, att_gp, count(distinct reg_cookie) as count from (
 select reg_cookie, service, att_level_up1, att_gp
 from `i-dss-ent-data.ent_summary.att_20160201_present`
 where reg_cookie in (
 select distinct reg_cookie
 from `i-dss-ent-data.ent_summary.att_20160201_present`
-where start_dt >= "2020-06-27" and start_dt <= "2020-07-27" and att_level_up1 = "NWSL" and service not in ("Trial","Trial to Paid")) and start_dt < "2020-06-27"
+where start_dt >= "2020-06-27" and start_dt <= "2020-07-27" and att_level_up1 = "NWSL" and service not in ("Trial","Trial to Paid")) and start_dt < "2020-06-27")
+group by 1,2
