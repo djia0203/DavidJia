171b7f76467a1ff94f6c356532462e65f067a4b9
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-08-03 17:13:21 -0400

    Auto-commit: Mon Aug 03 2020 17:13:07 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 2.sql
@@ -1,3 +1,11 @@
-select distinct video_series_nm, video_title, video_episode_nbr
-from `i-dss-ent-data.dw_vw.mpx_video_content`
-where (video_title like 'Match Replay%' or video_title like 'NWSL%') and video_series_nm = "National Women's Soccer League" and video_full_episode_ind = true
+select distinct CASE when post_evar69_desc IS NULL then post_visitor_id else post_evar69_desc end user,
+       case when post_multi_value_var3_desc like "%collapsed_upsell_web:collapsed%" and post_multi_value_var3_desc not like "%collapsed_upsell_web:anchor%" 
+                 and post_multi_value_var3_desc not like "%collapsed_upsell_web:control%" then "collapsed"
+            when post_multi_value_var3_desc like "%collapsed_upsell_web:anchor%" and post_multi_value_var3_desc not like "%collapsed_upsell_web:collapsed%" 
+                 and post_multi_value_var3_desc not like "%collapsed_upsell_web:control%" then "anchor"
+            when post_multi_value_var3_desc like "%collapsed_upsell_web:control%" and post_multi_value_var3_desc not like "%collapsed_upsell_web:anchor%" 
+                 and post_multi_value_var3_desc not like "%collapsed_upsell_web:collapsed%" then "control" 
+            else "pollution" end as segment,
+       post_evar19_desc
+from `i-dss-ent-data.dw_vw.omniture_event_cdm_cnetcbscomsite` a join `i-dss-ent-data.ent_vw.subscription_fct` b on a.post_evar69_desc = b.cbs_reg_user_id_cd
+where day_dt between '2020-07-15' and '2020-07-22' and post_multi_value_var3_desc like "%collapsed_upsell%" and a.post_evar1_desc like "cbscom_site%"
