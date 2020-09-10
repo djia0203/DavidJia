961f374f4fb85449527ec23404d966b05ea37a5c
djia0203 <67021950+djia0203@users.noreply.github.com>
2020-09-10 14:15:27 -0400

    Auto-commit: Thu Sep 10 2020 14:15:19 GMT-0400 (Eastern Daylight Time)

--- superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 3.sql
+++ superQuery/dawei.jia@cbsinteractive.com/My first dashboard/Tab 3.sql
@@ -1,3 +1,9 @@
-select reg_cookie, att_level_up1, service
-from `i-dss-ent-data.ent_summary.att_20160201_present`
-where start_dt >= "2020-06-27" and start_dt <= "2020-07-27" and att_level_up1 = "NWSL" and service not in ("Trial","Trial to Paid")
+SELECT day_dt, visit_session_id, post_visitor_id, replace(ftag,"ftag:","") as ftag, 
+       sum(case when exists (select * from unnest(post_event_id_list_desc) as x where x.name = 'Custom Event 76') then 1 else 0 end) as starts,
+       sum(case when exists (select * from unnest(post_event_id_list_desc) as x where x.name = 'Custom Event 52') then 1 else 0 end) as streams
+FROM  `i-dss-ent-data.dw_vw.omniture_event_cdm_cnetcbscomsite`, UNNEST(post_campaign_nm) as ftag
+WHERE  day_dt >= "2020-07-30" and day_dt <= '2020-08-31' 
+      -- and ftag in (SELECT distinct ftag FROM `i-dss-ent-data.temp_dj.Super_service_ftag` WHERE F3 = "BET")
+      and ftag like "AAM-00-10ajc%"
+group by 1,2,3,4
+order by 1,2,3,4 desc
